287e3d66c6dea335de146d8df7d4421b5f8f0029
indiandevtm <rajarockromeo@gmail.com>
2022-08-08 11:04:26 +0530

    removed coment

--- src/pages/Products/ProductListing.jsx
+++ src/pages/Products/ProductListing.jsx
@@ -9,17 +9,6 @@ const ProductListing = () => {
 
    const products = useSelector((state) => state);
    console.log("Products Listing page=====>", products)
-   // const fectchProduct = async () => {
-   //    const res = await axios
-   //       .get('https://fakestoreapi.com/products')
-   //       .catch((err) => {
-   //          console.log("Error:===========>", err);
-   //       });
-   //    console.log("API response data====>", res.data)
-   // };
-
-   // useEffect(() => {
-   // }, [fectchProduct]);
    
    return (
       <>
